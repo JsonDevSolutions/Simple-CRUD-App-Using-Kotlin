4f80d9dbfd49ade87f7326513ecd095786f16d37
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-09-20 15:38:37 +0800

    Update into view binding

--- app/src/main/java/com/example/viewmodel/LoginFragment.kt
+++ app/src/main/java/com/example/viewmodel/LoginFragment.kt
@@ -4,26 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.example.viewmodel.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val login = view?.findViewById<TextView>(R.id.btn_login)
-        login?.setOnClickListener{
+        val btnLogin = binding.btnLogin
+        btnLogin?.setOnClickListener{
             val action = LoginFragmentDirections.actionLoginFragmentToProductListFragment()
-            login?.findNavController()?.navigate(action)
+            btnLogin?.findNavController()?.navigate(action)
         }
+
+        val navRegister = binding.navRegister
+        navRegister?.setOnClickListener{
+            val action = LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+            navRegister?.findNavController()?.navigate(action)
+        }
+
         (requireActivity() as? AppCompatActivity)?.supportActionBar?.hide()
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        // Retrieve and inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
